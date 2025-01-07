0b7ce26a7fdcd2237c95f449917f387c868cb582
KhachAl <146882463+KhachAl@users.noreply.github.com>
2025-01-07 21:50:47 +0400

    update

--- migrations.py
+++ migrations.py
@@ -46,9 +46,36 @@ def add_columns_to_tables(db_name):
             connection.close()
 
 
+def create_indexes(db_name):
+    try:
+        connection = sqlite3.connect(db_name)
+        cursor = connection.cursor()
+
+
+        cursor.execute("""
+            CREATE INDEX IF NOT EXISTS idx_Преподаватель_Должность
+            ON Преподаватель (Должность);
+        """)
+        print("Индекс 'idx_Преподаватель_Должность' успешно создан или уже существует.")
+
+
+        cursor.execute("""
+            CREATE INDEX IF NOT EXISTS idx_Пара_Вид_занятия
+            ON Пара (Вид_занятия);
+        """)
+        print("Индекс 'idx_Пара_Вид_занятия' успешно создан или уже существует.")
+
+        connection.commit()
+    except sqlite3.Error as e:
+        print(f"Ошибка при создании индексов: {e}")
+    finally:
+        if connection:
+            connection.close()
+
+
 if __name__ == "__main__":
     DATABASE_NAME = "university.db"
 
 
     add_columns_to_tables(DATABASE_NAME)
-
+    create_indexes(DATABASE_NAME)
